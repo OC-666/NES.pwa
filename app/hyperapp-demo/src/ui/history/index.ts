19621871d842d228a2ec361c4dfbf145e79ac0f7
PPz <ppzreboot@qq.com>
2025-01-10 11:42:15 +0800

    feat: enhance history component with title

--- app/hyperapp-demo/src/ui/history/index.ts
+++ app/hyperapp-demo/src/ui/history/index.ts
@@ -3,8 +3,11 @@ import { type I_root_state } from '../../state'
 
 export
 const History = (history: string[]): VNode<I_root_state> =>
-  h('ul', {},
-    history.map(item =>
-      h('li', {}, text(item))
+  h('div', {}, [
+    h('h2', {}, text('History')),
+    h('ul', {},
+      history.map(item =>
+        h('li', {}, text(item))
+      )
     )
-  )
+  ])
