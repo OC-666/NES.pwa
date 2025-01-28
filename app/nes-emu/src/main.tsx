239fbad313cc8bb321faaf6b7a1d675d0e808212
PPz <ppzreboot@qq.com>
2025-01-28 18:03:27 +0800

    refactor: style on size and color

--- app/nes-emu/src/main.tsx
+++ app/nes-emu/src/main.tsx
@@ -20,6 +20,6 @@ function main() {
 
 function initialize() {
   const theme = app_state.user_settings.theme.get_state()
-  document.documentElement.style.setProperty('--fore-color', theme.fore_color.join(' '))
-  document.documentElement.style.setProperty('--back-color', theme.back_color.join(' '))
+  document.documentElement.style.setProperty('---fore-color', theme.fore_color.join(' '))
+  document.documentElement.style.setProperty('---back-color', theme.back_color.join(' '))
 }
--- lib/k2g/nes.tsx
+++ lib/k2g/nes.tsx
@@ -1,6 +1,7 @@
 import { D_pad } from './_common/d-pad'
 import { CTX_k2g, I_k2g_ctx } from './_common/context'
 import { Bind } from './_common/bind'
+import { Gamepad_btn } from './_common/button'
 
 export
 function NES_gamepad_map(props: I_k2g_ctx) {
@@ -9,8 +10,20 @@ function NES_gamepad_map(props: I_k2g_ctx) {
       <Bind />
       <div className='k2g nes-container'>
         <D_pad />
-        
+        <SS />
       </div>
     </CTX_k2g.Provider>
   )
+}
+
+/** Select & Start */
+function SS() {
+  return <div className='ss'>
+    <Gamepad_btn from='select' label_position='top'>
+      <span className='from'>select</span>
+    </Gamepad_btn>
+    <Gamepad_btn from='start' label_position='top'>
+      <span className='from'>start</span>
+    </Gamepad_btn>
+  </div>
 }
